f7dd2aa8e641ff85c249c72e2bde0a9ba9f27484
rizaljihadudin <mithnyong@gmail.com>
2025-05-09 22:44:22 +0700

    feat : our projects, why choose us, our services section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import AboutImg from '../../assets/images/about-us.jpg'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
+import ServiceImg from '../../assets/images/construction1.jpg'
+import ProjectImg from '../../assets/images/construction2.jpg'
+import Icon1 from '../../assets/images/icon-1.svg';
+import Icon2 from '../../assets/images/icon-2.svg';
+import Icon3 from '../../assets/images/icon-3.svg';
 
 const Home = () => {
   return (
@@ -48,6 +53,250 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+
+
+            {/* Our Services Section  */}
+            <section className='section-3 bg-light py-5'>
+                <div className="container-fluid py-5">
+                    <div className="section-header">
+                        <span>Our Services</span>
+                        <h2>Our construction services</h2>
+                        <p>We offer a diverse range of construction services to meet your needs</p>
+                    </div>
+                    <div className="row pt-4">
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ServiceImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ServiceImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ServiceImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ServiceImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Why Choose Us */}
+            <section className='section-4 py-5'>
+                <div className="container py-5">
+                    <div className="section-header">
+                        <span>Why Choose Us</span>
+                        <h2>Discover our wide variety of projects</h2>
+                        <p>
+                            Created in close partnership with our clients and collaborators, this approach merges industry expertise, <br/>
+                            decades of experience, innovations and flexibility to consistently deliver excellence.
+                        </p>
+                    </div>
+                    <div className="row pt-4">
+                        <div className="col-md-4">
+                            <div className="card shadow border-0 p-4">
+                                <div className="card-icon">
+                                    <img src={Icon1} alt="" />
+                                </div>
+                                <div className="card-title mt-3">
+                                    <h3>SweetHome Solutions</h3>
+                                </div>
+                                <p>
+                                    Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card shadow border-0 p-4">
+                                <div className="card-icon">
+                                    <img src={Icon2} alt="" />
+                                </div>
+                                <div className="card-title mt-3">
+                                    <h3>Superior Craftmanship</h3>
+                                </div>
+                                <p>
+                                    Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card shadow border-0 p-4">
+                                <div className="card-icon">
+                                    <img src={Icon3} alt="" />
+                                </div>
+                                <div className="card-title mt-3">
+                                    <h3>Knowledge and Expertise</h3>
+                                </div>
+                                <p>
+                                    Small actions create big impacts. It all begins and ends with each employee committing to safer work practices daily, ensuring they return home safely.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            {/* Our Projects Section  */}
+            <section className='section-3 bg-light py-5'>
+                <div className="container-fluid py-5">
+                    <div className="section-header">
+                        <span>Our Projects</span>
+                        <h2>Discover our diverse range of projects</h2>
+                        <p>We offer a diverse range of construction services to meet your needs</p>
+                    </div>
+                    <div className="row pt-4">
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Jakarta Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-lg-3">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectImg} className='w-100' />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Speciality Construction</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        </p>
+                                    </div>
+                                    <a href="" className='btn btn-primary'>Read More</a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
         </main>
 
         <Footer />
