016a4db30848f64329b6e5cb9be55dca566c9380
rizaljihadudin <mithnyong@gmail.com>
2025-05-20 19:45:18 +0700

    feat : service section

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 import About from './layout/About'
@@ -16,26 +16,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { baseUrl } from '../../config'
+import LatestServices from './layout/LatestServices'
 
 
 const Home = () => {
-
-    const [services, setServices] = useState([]);
-
-    const fetchLastServices = async () => {
-         const res = await fetch(`${baseUrl}/get-latest-services?limit=4`,{
-            method : 'GET'
-        }); 
-        
-        const result = await res.json();
-        setServices(result.data);
-    }
-
-    useEffect(() => {
-        fetchLastServices();
-    }, [])
-
     return (
         <>
             <Header />
@@ -65,43 +49,7 @@ const Home = () => {
 
 
                 {/* Our Services Section  */}
-                <section className='section-3 bg-light py-5'>
-                    <div className="container-fluid py-5">
-                        <div className="section-header">
-                            <span>Our Services</span>
-                            <h2>Our construction services</h2>
-                            <p>We offer a diverse range of construction services to meet your needs</p>
-                        </div>
-                        <div className="row pt-4">
-                            {
-                                services &&services.map((service, index) => {
-                                    return (
-                                        <div className="col-md-3 col-lg-3" key={index}>
-                                            <div className="item">
-                                                <div className="service-image">
-                                                    <img src={ServiceImg} className='w-100' />
-                                                </div>
-                                                <div className="service-body">
-                                                    <div className="service-title">
-                                                        <h3>{service.title.toUpperCase()}</h3>
-                                                    </div>
-                                                    <div className="service-content">
-                                                        <p>
-                                                            {service.short_desc}
-                                                        </p>
-                                                    </div>
-                                                    <a href="" className='btn btn-primary'>Read More</a>
-                                                </div>
-
-                                            </div>
-                                        </div> 
-                                    )
-                                })
-                            }
-                            
-                        </div>
-                    </div>
-                </section>
+                <LatestServices/>
 
                 {/* Why Choose Us */}
                 <section className='section-4 py-5'>
